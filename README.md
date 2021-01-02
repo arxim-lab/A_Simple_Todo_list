c585e340e14e4b6822b7446f201b5481c44e61ef
arxim-lab <sxh910@gmail.com>
2021-01-02 15:46:05 +0800

    Fix some problems

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## A Simple TO-DO List
 
-> 一个简单的命令行版 TO-DO List App,使用Bash脚本实现
+> 一个使用Bash脚本实现的简单 TO-DO List App
 
 ### First Time
 
@@ -12,35 +12,55 @@
 > 
 > ~~尚未测试Windows PowerShell对脚本的兼容性~~
 
+从Github中下载脚本的用户可执行`install`指令将脚本安装到系统目录，使用`uninstall`指令卸载脚本
+
+```bash
+wget https://github.com/arxim-lab/A_Simple_Todo_list/raw/main/todo.sh | sudo ./todo.sh install
+```
+
+安装完成后可直接使用`todo`操作
+
+```bash
+todo addlist first_list
+todo add first_event
+todo check
+```
+
 ### Usage
 
-可通过`./todo.sh help`指令查看帮助信息
+可通过`help`指令查看帮助信息
 
-- `addlist ListName` 添加一个名为ListName的TO-DO list
+##### 指令列表
+
+- `addlist (ListName)` 添加一个名为ListName的TO-DO list，默认名为Default
 
   list名称只能包含大小写英文字母、数字、英文短横线`-`以及英文下划线`_`！
 
-- `select ListName` (快捷指令:`s`) 选择名为ListName的TO-DO list
+- `select (ListName)` (快捷指令:`s`) 选择名为ListName的TO-DO list
 
   在对TO-DO list内事件的操作前必须先进行选择操作，可单独使用`select`或`s`查看当前选中的list
 
 - `add SthToDo` (快捷指令:`a`) 在当前list内添加一条事件，内容为SthToDo
 
-- `check` (快捷指令:`ck`) 查看当前list内所有的事件
+- `check` (快捷指令:`c`) 查看当前list内所有的事件
 
 - `done TodoID` (快捷指令:`d`) 标记当前list内ID为TodoID的事件为已完成
 
   TodoID为执行check指令后对应事件前标注的数字
 
-- `clear` 彻底删除当前list内所有已完成的事件
+- `undo TodoID` (快捷指令:`u`) 标记当前list内ID为TodoID的事件为未完成
 
+- `clear (ListName)` 彻底删除当前list内所有已完成的事件，可附加ListName指定list
 - `delete` 彻底删除已选中的整个list
+- `delete TodoID` 删除当前list内ID为TodoID的事件
+- `list` (快捷指令:`l`)查看本地保存的所有list
+- `editraw` 使用系统编辑器打开数据文件 (手动编辑)
+- `backup (/path/to/file)`  将数据文件备份到指定位置，默认为~/TODO_LIST.bak
+- `recover /path/to/file` 从指定位置恢复数据文件
+- `help` (快捷指令:`h`) 查看该帮助信息
+- `uninstall` 卸载该脚本并删除数据文件 (仅限shell安装)
 
-- `list` 查看本地保存的所有list
-
-- `editraw` 使用VIM打开数据文件 (手动编辑)
-
-- `help` 查看该帮助信息
+> 推荐搭配Yakuake使用，效果更佳。
 
 ### Screenshots
 
@@ -52,22 +72,31 @@
 
 ### Known Issues
 
-- 事件内容不能包含空格，否则会导致事件录入错误或显示错误 `2020.1.2`
-- 假如list中存在同名事件，clear时会被一并清除 `2020.1.2`
+`2020.1.2`
+
+目前已知的程序中存在的 ~~features~~ BUGS :
+
+- 事件内容不能包含空格，否则会导致事件录入错误或显示错误
+- 假如list中存在同名事件，clear时会被一并清除
+- 事件添加操作add目前还不能检查输入合法性
 
 ### TO-DOs
 
-- [ ] 修正：脚本帮助信息
-- [ ] 修正：改check的快捷指令为c
-- [ ] 完善程序功能：delete、clear等
-- [ ] 添加新功能：自动安装&自动卸载
-- [ ] 添加新功能：备份功能
-- [ ] 添加新功能：无给派指令时执行check操作
-- [ ] 添加新功能：事件执行倒计时&执行时限（deadline功能）
+- [ ] 添加新功能：事件执行倒计时&执行时限（Deadline）以及提醒功能
+- [ ] 添加新功能：config
+- [ ] 丰富显示效果
 - [ ] 修复BUG：事件内容不能包含空格
 - [ ] 修复BUG：同名事件会被删除
+- [ ] 修复BUG：add输入合法性
 
 ### Update Information
 
 - v0.0.1_20200102 - First Version
   - 第一个版本
+
+- v0.1.1_20200102 - Fix
+  - 修正了脚本帮助信息
+  - 修正了check的快捷指令为c
+  - 完善了delete、clear功能
+  - 添加了undo、install、uninstall、backup、recover等功能
+  - 无指令给派时默认执行check操作
--- todo.sh
+++ todo.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
+echo -e "\e[32mINFO\e[0m:  This application has not been installed.\n       Try 'install' for installation."
+if [[ ${1} = 'install' ]]; then
+    cp ${0} ./.todo_exec;
+    sed '2,13d' ./.todo_exec -i
+    sed '/^COMMANDS=/d' ./.todo_exec -i
+    sed 's/^##@onekeysignal#&//g' ./.todo_exec -i
+    sudo cp ./.todo_exec /usr/bin/todo
+    rm ./.todo_exec
+    sudo chmod +x /usr/bin/todo
+    echo Installed to /usr/bin/todo
+    exit
+fi
 #
 #Usage:
 # todo [COMMANDS] [OPTIONS] [arguments] 
 #
 #Commands:
-#    createlist ListName
-#  s|select ListName
-#  c|create (-l ListName) SthToDo
-# ck|check (ListName)
-#  d|done TodoID
-#    clear
-#    delete TodoID
-#    delete ListName
-#    list
-#    help
+#    addlist    ListName
+#  s|select     (ListName)
+#  a|add        SthToDo
+#  c|check      (ListName)
+#  d|done       TodoID
+#  u|undo       TodoID
+#    clear      (ListName)
+#  d|delete     TodoID
+#    delete
+#  l|list
+#  h|help
 #    config [key] [value]
 #    editraw
-#
-#Global Options
-# -c /path/to/file
+#    backup     (FilePath)
+#    recover    FilePath
 #
 #Config:
 # undobgcolor
@@ -36,51 +48,58 @@ done
 unset para_
 unset i
 
-COMMANDS=(addlist select add check done clear delete list help config editraw)
+COMMANDS=(addlist select add check done undo clear delete list help editraw backup recover)
+##@onekeysignal#&COMMANDS=(addlist select add check done undo clear delete list help editraw backup recover uninstall)
 
 MSG_INIT() {
-ERROR_MSG[1]="Usage: todo [COMMANDS] [OPTIONS] [arguments]\nTry 'help' for more information."
+
 ERROR_MSG[2]="Unknown Command: ${Para_[1]}\nUse 'help' for a list of commands."
 ERROR_MSG[3]="No TODO list has been selected.\nUse 'select' or add '-l' option for selection.\nTry 'help' for more information"
-ERROR_MSG[4]="List existed."
-ERROR_MSG[5]="List not existed."
+ERROR_MSG[4]="List $([[ ${Para_[2]} != '' ]] && echo ${Para_[2]} || echo 'Default') is existed."
+ERROR_MSG[5]="List $([[ ${Para_[2]} != '' ]] && echo ${Para_[2]} || echo 'Default') is not existed."
 ERROR_MSG[6]="Unsupported character.\nList name may only contain alphanumeric characters, hyphens, and underscore."
 ERROR_MSG[7]="Blank characters are not supported.\nCheck your input and try again."
 ERROR_MSG[8]="Event ${Para_[2]} is not existed or has been finished."
+ERROR_MSG[9]="Event ${Para_[2]} is not existed or unfinished."
+ERROR_MSG[10]="Event ${Para_[2]} is not existed."
+ERROR_MSG[11]="This event is empty."
+ERROR_MSG[12]="FilePath is necessary when you perform recover operation."
 
 WARN_MSG[1]="It will delete the selected TODO list from the TODOList file.\n       This action is unrecoverable."
+WARN_MSG[2]="It will remove all the data on the existed data file.\n       This action is unrecoverable."
 
 HELP_MSG="Usage: todo [COMMANDS] [OPTIONS] [arguments]\n\n\
 Commands:\n\
-   addlist    ListName        Create a new TODO list and select it\n\
- s|select     ListName        Select a TODO list for further action\n\
- a|add        SthToDo         Add a new event to the selected list\n\
-ck|check                      Display all the TODO list in the file\n\
- d|done       TodoID          Sign the event as finished\n\
-   clear                      Delete all the finished events in the list\n\
-   delete     TodoID          Delete the event by force\n\
-   delete                     Delete the TODO list\n\
-   list                       Display all the events in the list\n\
-   help                       Display this help text and exit\n\
-   config [key] [value]\n\
-   editraw                    Open the TODOlist file with VIM text editor\n\n\
-Global Options\n\
--c /path/to/list\n\
--l ListName\n\n\
-Config:\n\
- undobgcolor\n\s
- undocolor\n\
- donebgcolor\n\
- donecolor\n"
+ addlist\t(ListName)\tCreate a new TODO list and select it\n\
+ s|select\t(ListName)\tSelect a TODO list for further action\n\
+ a|add\t\tSthToDo\t\tAdd a new event to the selected list\n\
+ c|check\t(ListName)\tDisplay all the events in the list or the list offered\n\
+ d|done\t\tTodoID\t\tSign the event as finished\n\
+ u|undo\t\tTodoID\t\tSign the event as unfinished\n\
+ clear\t\t(ListName)\tDelete all the finished events in the list or the list offered\n\
+ delete\t\tTodoID\t\tDelete the event by force\n\
+ delete\t\t\t\tDelete the TODO list\n\
+ l|list\t\t\t\tDisplay all the TODO list in the file\n\
+ h|help\t\t\t\tDisplay this help text and exit\n\
+ editraw\t\t\tOpen the TODOlist file with VIM text editor\n\
+ backup\t\t(FilePath)\tBackup the data file to filepath or ~/TODO_List.bak\n\
+ recover\tFilePath\tRecover the data file from filepath\n\n\
+"
 
-CL_MSG="Created a new TODO list named ${Para_[2]}."
-SL_MSG="Selected the list named ${Para_[2]}."
+CL_MSG="Created a new TODO list named $([[ ${Para_[2]} != '' ]] && echo ${Para_[2]} || echo 'Default')."
+SL_MSG="Selected the list named $([[ ${Para_[2]} != '' ]] && echo ${Para_[2]} || echo ${TODO_SELECTED_NOW})."
 AE_MSG="Added a new event in List ${TODO_SELECTED_NOW}.\n${Para_[2]}"
+BK_MSG="Data file has been copied to $([[ ${Para_[2]} != '' ]] && echo ${Para_[2]} || echo ~/TODO_LIST.bak)."
+RC_MSG="Data file has been recovered."
 }
 
 # err Code
 err() {
-    echo -e "\e[31mERROR\e[0m: ${ERROR_MSG[$1]}"
+    if [[ ${1} != 1 ]]; then
+        echo -e "\e[31mERROR\e[0m: ${ERROR_MSG[$1]}"
+    else
+        echo -e ${ERROR_MSG[$1]}
+    fi
     exit $1
 }
 
@@ -98,19 +117,6 @@ warn() {
     fi
 }
 
-# readVar inParameter inVar
-readVar() {
-    local inVar=$2
-    local inParameter=$1
-    for p_ in $(seq ${#Para_[*]}); do
-        if [[ ${Para_[${p_}]} = ${inParameter} ]]; then
-            export ${inVar}=${Para_[$(expr ${p_} + 1 )]}
-            break
-        fi
-    done
-    unset p_s
-}
-
 # checkNow inSelected
 checkNow() {
     if [[ ${1} = '' ]]; then
@@ -121,42 +127,22 @@ checkNow() {
     return ${?}
 }
 
-# addtionalSelect
-addtionalSelectCheck() {
-    readVar -l AddtionalSelectedList
-    if [[ ${AddtionalSelectedList} != '' ]]; then
-        checkNow ${AddtionalSelectedList}
-        if [[ ${?} == 0 ]]; then
-            echo ${AddtionalSelectedList} > ${TODO_SELECTED_FILE}
-            TODO_SELECTED_NOW=${AddtionalSelectedList}
-            unset AddtionalSelectedList
-            return 0
-        fi
-    else
-        unset AddtionalSelectedList
-        return 1
-    fi
-}
-
 # selectCheck
 selectCheck() {
-    addtionalSelectCheck
+    checkNow
     if [[ ${?} != 0 ]]; then
-        checkNow
-        if [[ ${?} != 0 ]]; then
-            if [[ ${TODO_SELECTED_NOW} = '' ]]; then
-                err 3
-            else
-                echo '' > ${TODO_SELECTED_FILE}
-                err 5
-            fi
+        if [[ ${TODO_SELECTED_NOW} = '' ]]; then
+            err 3
+        else
+            echo '' > ${TODO_SELECTED_FILE}
+            err 5
         fi
     fi
 }
 
 Todo_addlist() {
     checkNow ${Para_[2]}
-    if [[ ${?} != 0 ]]; then
+    if [[ ${?} != 0 && ${Para_[2]} != '' ]]; then
         echo ${Para_[2]} | grep -E "^[A-Za-z0-9_-]+\$" > /dev/null
         if [[ ${?} == 0 ]]; then
             echo ${Para_[2]} >> ${TODO_LISTS_FILE}
@@ -165,6 +151,15 @@ Todo_addlist() {
         else
             err 6
         fi
+    elif [[ ${Para_[2]} = '' ]]; then
+        checkNow 'Default'
+        if [[ ${?} != 0 ]]; then
+            echo 'Default' >> ${TODO_LISTS_FILE}
+            echo -e ${CL_MSG}
+            echo 'Default' > ${TODO_SELECTED_FILE}
+        else
+            err 4
+        fi
     else
         err 4
     fi
@@ -172,28 +167,39 @@ Todo_addlist() {
 Todo_select() {
     if [[ ${Para_[2]} = '' ]]; then
         selectCheck
-        echo -e "Selected a list named ${TODO_SELECTED_NOW}."
     else
         checkNow ${Para_[2]}
         if [[ ${?} == 0 ]]; then
             echo ${Para_[2]} > ${TODO_SELECTED_FILE}
             TODO_SELECTED_NOW=${Para_[2]}
-            echo -e ${SL_MSG}
         else
             err 5
         fi
     fi
+    echo -e ${SL_MSG}
 }
 Todo_add() {
     selectCheck
+    # 无法进行输入验证
+    if [[ ${Para_[2]} = '' ]]; then err 11; fi
     echo "${TODO_SELECTED_NOW}#\"${Para_[2]}\"" >> ${TODO_LISTS_FILE}
     echo -e ${AE_MSG}
 }
 Todo_check() {
-    # 不能做到Event内容中加入空格
-    selectCheck
-    local EventList_raw=($(cat ${TODO_LISTS_FILE} | grep -E "^(@)?${TODO_SELECTED_NOW}#"))
 #     local EventList_raw=($(cat ${TODO_LISTS_FILE} | grep -E "^(@)?${TODO_SELECTED_NOW}#" | awk -F'#"' '{print $2}' | sed 's/"$//g' | sed 's/"/\\"/g' | awk '{print "\""$0"\""}'| sed 's/\n/\ /g'))
+    # 不能做到Event内容中加入空格
+    if [[ ${Para_[2]} = '' ]]; then
+        selectCheck
+    else
+        checkNow ${Para_[2]}
+        if [[ ${?} == 0 ]]; then
+            echo ${Para_[2]} > ${TODO_SELECTED_FILE}
+            TODO_SELECTED_NOW=${Para_[2]}
+        else
+            err 5
+        fi
+    fi
+    local EventList_raw=($(cat ${TODO_LISTS_FILE} | grep -E "^(@)?${TODO_SELECTED_NOW}#"))
     for i_ in $(seq ${#EventList_raw[*]}); do
         i_=$(expr ${i_} - 1)
         echo ${EventList_raw[$i_]} | grep -E "^@" > /dev/null
@@ -217,22 +223,57 @@ Todo_done() {
     if [[ ${isIDaN} != 0 || ${isDone} == 0 || ${EventList_raw[${Para_[2]}]} = '' ]]; then
         err 8
     fi
-    # 假如存在同名Event,会被一同清除
+    # 假如存在同名Event,会被一同done
     sed "s/^${EventList_raw[${Para_[2]}]}$/@${EventList_raw[${Para_[2]}]}/g" ${TODO_LISTS_FILE} -i
-    echo finished
+    echo Finished
+}
+Todo_undo() {
+    # Undo存在与Done同样的问题
+    selectCheck
+    local EventList_raw=($(cat ${TODO_LISTS_FILE} | grep -E "^(@)?${TODO_SELECTED_NOW}#"))
+    echo ${Para_[2]} | grep -E "^[0-9]+\$" > /dev/null
+    local isIDaN=${?}
+    echo ${EventList_raw[${Para_[2]}]} | grep -E "^@" > /dev/null
+    local isDone=${?}
+    if [[ ${isIDaN} != 0 || ${isDone} == 1 || ${EventList_raw[${Para_[2]}]} = '' ]]; then
+        err 9
+    fi
+    # 假如存在同名Event,会被一同Undo
+    sed "s/^${EventList_raw[${Para_[2]}]}$/${EventList_raw[${Para_[2]}]#@}/g" ${TODO_LISTS_FILE} -i
+    echo Finished
 }
 Todo_clear() {
-    selectCheck
+    if [[ ${Para_[2]} = '' ]]; then
+        selectCheck
+    else
+        checkNow ${Para_[2]}
+        if [[ ${?} == 0 ]]; then
+            echo ${Para_[2]} > ${TODO_SELECTED_FILE}
+            TODO_SELECTED_NOW=${Para_[2]}
+        else
+            err 5
+        fi
+    fi
     sed "/^@${TODO_SELECTED_NOW}#/d" ${TODO_LISTS_FILE} -i
-    echo finished
+    echo Finished
 }
 Todo_delete() {
     selectCheck
+    if [[ ${Para_[2]} != '' ]]; then
+        local EventList_raw=($(cat ${TODO_LISTS_FILE} | grep -E "^(@)?${TODO_SELECTED_NOW}#"))
+        echo ${Para_[2]} | grep -E "^[0-9]+\$" > /dev/null
+        if [[ ${?} == 0 && ${EventList_raw[${Para_[2]}]} != '' ]]; then
+            sed -E "/^${EventList_raw[${Para_[2]}]}$/d" ${TODO_LISTS_FILE} -i
+            return
+        else
+            err 10
+        fi
+    fi
     warn 1
     if [[ ${?} == 0 ]]; then
         echo "deleting the selected TODO list '${TODO_SELECTED_NOW}'..."
         sed -E "/^(@)?${TODO_SELECTED_NOW}/d" ${TODO_LISTS_FILE} -i
-        echo finished
+        echo Finished
         echo '' > ${TODO_SELECTED_FILE}
         echo "Selection Clear"
     else
@@ -255,7 +296,35 @@ Todo_help() {
 # }
 Todo_editraw() {
     vim ${TODO_LISTS_FILE}
+    if [[ $? != 0 ]]; then nano ${TODO_LISTS_FILE}; fi
+    if [[ $? != 0 ]]; then vi ${TODO_LISTS_FILE}; fi
 }
+Todo_backup() {
+    if [[ ${Para_[2]} = '' ]]; then
+        cp ${TODO_LISTS_FILE} ~/TODO_LIST.bak
+    else
+        cp ${TODO_LISTS_FILE} ${Para_[2]}
+    fi
+    echo -e ${BK_MSG}
+}
+Todo_recover() {
+    if [[ ${Para_[2]} = '' ]]; then
+        err 12
+    else
+        warn 1
+        if [[ ${?} == 0 ]]; then
+            cp ${Para_[2]} ${TODO_LISTS_FILE}
+            echo -e ${RC_MSG}
+        else
+            echo Cancelled
+        fi
+    fi
+}
+##@onekeysignal#&Todo_uninstall() {
+##@onekeysignal#&    sudo rm /usr/bin/todo
+##@onekeysignal#&    rm ${TODO_LISTS_FILE}
+##@onekeysignal#&    rm ${TODO_SELECTED_FILE}
+##@onekeysignal#&}
 
 # Initialize
 #Checking selected list
@@ -270,7 +339,6 @@ MSG_INIT
 
 # Execute Command
 #Checking TODO list file
-readVar -c TODO_LISTS_FILE
 if [[ ${TODO_LISTS_FILE} = '' ]]; then
     ls -a ~ | grep '.todo_list' > /dev/null
     if [[ $? != 0 ]]; then
@@ -279,9 +347,6 @@ if [[ ${TODO_LISTS_FILE} = '' ]]; then
     TODO_LISTS_FILE=~/.todo_list
 fi
 
-if [[ ${Para_[1]} = '' ]]; then
-    err 1
-fi
 isCMDDone=0
 for cmd_ in ${COMMANDS[*]}; do
     if [[ ${cmd_} = ${Para_[1]} ]]; then
@@ -292,12 +357,14 @@ done
 unset cmd_
 
 # Alias
-if [[ ${Para_[1]} = 'a' ]];  then isCMDDone=1; Todo_add; fi
-if [[ ${Para_[1]} = 's' ]];  then isCMDDone=1; Todo_select; fi
-if [[ ${Para_[1]} = 'ck' ]]; then isCMDDone=1; Todo_check; fi
-if [[ ${Para_[1]} = 'd' ]];  then isCMDDone=1; Todo_done; fi
+if [[ ${Para_[1]} = 'a' ]]; then isCMDDone=1; Todo_add; fi
+if [[ ${Para_[1]} = 's' ]]; then isCMDDone=1; Todo_select; fi
+if [[ ${Para_[1]} = 'c' ]]; then isCMDDone=1; Todo_check; fi
+if [[ ${Para_[1]} = 'd' ]]; then isCMDDone=1; Todo_done; fi
+if [[ ${Para_[1]} = 'u' ]]; then isCMDDone=1; Todo_undo; fi
+if [[ ${Para_[1]} = 'l' ]]; then isCMDDone=1; Todo_list; fi
+if [[ ${Para_[1]} = 'h' ]]; then isCMDDone=1; Todo_help; fi
 
-if [[ ${isCMDDone} == 0 ]]; then
-    err 2
-fi
+if [[ ${isCMDDone} == 0 ]]; then Todo_check; fi
+
 unset isCMDDone
